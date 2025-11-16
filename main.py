7f6f588948bec5ad590ece51f76a26ac89a2a86e
AndyB404 <abrice007@gmail.com>
2025-11-16 13:49:22 +0000

    Implement CLI prompt with multiline support and error handling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,10 +10,18 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    print("Hello from ai-agent-project!")
+    if len(sys.argv) < 2:
+        print("ERROR: Missing prompt")
+        exit(1)
+    prompt = " ".join(sys.argv[1:])
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        contents=messages
     )
     print(response.text)
     usage = response.usage_metadata
